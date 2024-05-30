17d0a24396a87f5d2a21166d1e12128f58504c1f
Deyao Chen <chendeyao000@gmail.com>
2024-05-30 12:06:31 +0000

    fix bugs

--- run.sh
+++ run.sh
@@ -12,13 +12,37 @@ usage() {
     exit 1
 }
 
+
+# Default number of cores
+num_cores=1
+
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -j|--num-cores)
+      num_cores="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      usage
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+# Restore positional parameters
+set -- "${POSITIONAL_ARGS[@]}"
+
 # Ensure at least 5 positional arguments are provided
 if [ "$#" -lt 5 ]; then
     usage
 fi
-
-# Default number of cores
-num_cores=1
 
 # Parse positional arguments
 executable="$1"
@@ -28,24 +52,19 @@ num_points="$4"
 IFS=',' read -r -a values <<< "$5"
 shift 5
 
-# Parse optional arguments
-while getopts "j:" opt; do
-    case $opt in
-        j)
-            num_cores="$OPTARG"
-            ;;
-        *)
-            usage
-            ;;
-    esac
-done
+export executable
+export input_file
+export dimension    
+export num_points
 
 # Define the function that will run the command
 command_function() {
     local value=$1
-    ${executable} ${input_file} ${dimension} ${num_points} ${value} subset_${value}.txt 2>&1 | ts > log_${value}.txt
+    echo ${executable}
+    SHIFT_TRIES=5000 ${executable} ${input_file} ${dimension} ${num_points} ${value} subset_${value}.txt 2>&1 | ts > log_${value}.txt
 }
 
+echo $num_cores
 
 # Export the function if parallel execution is used
 if [ "$num_cores" -gt 1 ]; then
