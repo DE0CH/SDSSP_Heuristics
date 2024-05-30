4d3f87ebaba5e49d36269f3f96182f2372245673
Deyao Chen <chendeyao000@gmail.com>
2024-05-30 13:56:05 +0000

    fix again

--- again.sh
+++ again.sh
@@ -2,9 +2,9 @@
 
 # Function to show usage
 usage() {
-    echo "Usage: $0 executable input_file dimension values [-j num_cores]"
+    echo "Usage: $0 executable csv_file dimension values [-j num_cores]"
     echo "  executable   The executable to run (e.g., ./a.out)"
-    echo "  input_file   Input file name (e.g., df_crit.txt)"
+    echo "  csv_file   Input file name (e.g., df_crit.csv)"
     echo "  num_points   Number of points in the input file (e.g., 2188)"
     echo "  dimension    Dimension value (e.g., 3)"
     echo "  values       A comma-separated list of values (number of points in the subset) (e.g., 40,60,80,90)"
@@ -45,7 +45,7 @@ fi
 
 # Parse positional arguments
 executable="$1"
-input_file="$2"
+csv_file="$2"
 num_points="$3"
 dimension="$4"
 IFS=',' read -r -a values <<< "$5"
@@ -53,17 +53,17 @@ shift 5
 
 # Export variables needed for the command function
 export executable
-export input_file
+export csv_file
 export dimension
 export values
 
 # Define the function that will run the command
 command_function() {
     local value=$1
-    python3 ../match_index.py ${input_file} subset_${value}.txt /dev/null subset_complement_${value}.csv
-    python3 ../extract_cvs.py subset_complement_${value}.csv subset_complement_${value}.txt
-    SHIFT_TRIES=5000 ${executable} 3 $ subset_complement_${value}.txt 2>&1 | ts > log_subset_complement_${value}.txt
-    python3 ../match_index.py "$csv_file" "subset_complement_${value}.txt" "subset_complement_${value}.csv" /dev/null 
+    python3 ../match_index.py ${csv_file} subset_${value}.txt /dev/null subset_complement_${value}.csv
+    python3 ../extract_csv.py subset_complement_${value}.csv subset_complement_${value}.txt
+    SHIFT_TRIES=5000 ${executable} subset_complement_${value}.txt ${dimension} $(expr ${num_points} - ${value}) ${value} subset_complement_subset_${value}.txt 2>&1 | ts > log_subset_complement_${value}.txt
+    python3 ../match_index.py ${csv_file} "subset_complement_${value}.txt" "subset_complement_${value}.csv" /dev/null 
     head -n 1 "subset_${value}.txt" | python3 -c 'import sys; import re; print(f"k={sys.argv[1]}", re.search(r"discrepancy=([\d\.]+)", input())[0])' "${value}"
 }
 
